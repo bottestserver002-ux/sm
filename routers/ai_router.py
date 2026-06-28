50f57de9b13b056e698d56c0181e770e22465b9b
ManhTruong-SsE <nguyentruong2003ct@gmail.com>
2026-06-29 00:23:16 +0700

    caption

--- routers/ai_router.py
+++ routers/ai_router.py
@@ -11,9 +11,7 @@ genai.configure(
     api_key=GEMINI_API_KEY
 )
 
-model = genai.GenerativeModel(
-    "gemini-1.5-flash"
-)
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 @router.post("/ask-ai")
